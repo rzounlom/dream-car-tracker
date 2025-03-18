ef19409b54ed5daef36b3136e633214f6aae77a8
rzounlom <rzounlom@gmail.com>
2025-03-18 08:13:20 -0400

    add Navbar comopnent

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/">DCT</a>
+      </div>
+      <div className="navbar-menu">
+        <ul>
+          <li>
+            <a href="/#cars">Cars</a>
+          </li>
+          <li>
+            <a href="#">Add Car</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
